36c8dc58aa01ce40b95da42cf5a7c22653b53871
SANTOS462 <whsantos2013@gmail.com>
2024-09-25 21:56:25 -0300

    Novos Métodos Somar par inverter

---
+++ Classes/ClassesMetodos.java
@@ -0,0 +1,37 @@
+
+public class ClassesMetodos {
+  
+   //Metodo 1: calcula a soma de 2 valores:	
+public int soma(int a, int b) {	
+	  return a + b;
+	
+}	
+	
+//Metodo 2: Verificar se um numero é Par:
+public boolean numero % 2 ==0;
+
+ // Este método executa outros metodos 
+public static void main(String[] args) {
+ //Instanciando a nossa que criamos
+	ClassesMetodos util = new ClassesMetodos();
+   // Este método executa outros métodos!
+
+public static void main(String[] args) {
+     //Intanciando a nossa que criamos:
+	ClassesMetodos util= new ClassesMetodos();
+	//Chamando os métodos:
+	System.out.println("Soma de 5 e 3: "+ util.soma(5, 3) );
+	System.out.println("O número 4 é par?" + util.ehPar(4));
+	
+}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/java2024/NumeroPar.java
@@ -0,0 +1,19 @@
+package java2024;
+
+public class NumeroPar {
+
+public statc void main(String[] args);
+
+ //Numero ver se é par 
+public boolean ehPar(int numero) {
+return numero % 2 ==0;
+
+}
+
+ //Chamando metodo:
+System.out.println("Soma de 5 e 3:"+util.soma(5, 3));
+System.out.println("O número 4 é par?" + util.ehPar(4));
+
+
+
+}
