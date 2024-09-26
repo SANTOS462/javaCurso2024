b8b1cfadedd7cd631def5982081d90d823c6c04f
SANTOS462 <whsantos2013@gmail.com>
2024-09-26 18:55:59 -0300

    Usando Métodos

--- Classes/ClassesMetodos.java
+++ Classes/ClassesMetodos.java
@@ -8,21 +8,44 @@ public int soma(int a, int b) {
 }	
 	
 //Metodo 2: Verificar se um numero é Par:
-public boolean numero % 2 ==0;
+public  
+boolean numero % 2 ==0;
 
- // Este método executa outros metodos 
-public static void main(String[] args) {
- //Instanciando a nossa que criamos
+
+public String invertString(String texto) {	
+	  return new StringBuilder(texto).reverse().toString();
+	
+}
+	
+ // Método 4: Calcular o fatorial de um número:
+public int fatorial(int numero) {
+ if (numero== 0 || numero ==1) {
+return 1;
+ }
+
+ return numero * fatorial(numero - 1
+ 
+}
+
+//Este método executa outros métodos!
+ public static void main(String[] args) {
+
+	
+	 
+	 
+	 //Instanciando a nossa que criamos
+
 	ClassesMetodos util = new ClassesMetodos();
    // Este método executa outros métodos!
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
      //Intanciando a nossa que criamos:
 	ClassesMetodos util= new ClassesMetodos();
 	//Chamando os métodos:
 	System.out.println("Soma de 5 e 3: "+ util.soma(5, 3) );
 	System.out.println("O número 4 é par?" + util.ehPar(4));
-	
+	System.out.println("String invertida de 'java': " +
+util.invertString("Java"));
 }
 }
 
